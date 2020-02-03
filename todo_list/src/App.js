ecab1094bece19397d5d0c86e3b8ba1ef24b5f51
shoaibfy <shoaibfy@gmail.com>
2020-02-03 14:17:13 +0530

    image added

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -3,54 +3,56 @@ import React, { Component } from 'react';
 import './App.css';
 
 
-class Todo extends Component{
-  constructor(props){
-  super(props);
-  this.state={
-    text:""
+class Todo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ""
+
+    }
+
+    this.sendInput = this.sendInput.bind(this);
+    this.takeInput = this.takeInput.bind(this);
+  }
+
+  sendInput() {
+    this.props.takeContext(this.state.text);
+    this.setState({
+      text: ""
+    })
+  }
+
+  takeInput(value) {
+    this.setState({
+      text: value
+    });
 
   }
 
-  this.sendInput=this.sendInput.bind(this);
-  this.takeInput=this.takeInput.bind(this);
-}
-
-sendInput(){
-  this.props.takeContext(this.state.text);
-  this.setState({
-    text:""
-  })
-}
-
-takeInput(value){
-this.setState({
-  text:value
-});
-
-}
-
 
   render() {
+    {console.log("This is TODO component")}
     return (
       <div className="todo"> Adding todo list details:
+      
         <div className="todo1">
           <input
-           type="text"
-           onChange={event =>this.takeInput(event.target.value)}
-           value={this.state.text}
-           maxLength={100}/>
-           <div>
-          <button 
-          type="submit"
-          onClick={this.sendInput}
-          value={this.state.text}>ADD
+            type="text"
+            onChange={event => this.takeInput(event.target.value)}
+            value={this.state.text}
+            maxLength={100} />
+          <div>
+            <button
+              type="submit"
+              onClick={this.sendInput}
+              value={this.state.text}>ADD
           </button>
-         
+
           </div>
-        </div>  
+        </div>
         <div>
-          Total charecters:{100-((this.state.text).length)}
-        </div>      
+          Total charecters:{100 - ((this.state.text).length)}
+        </div>
       </div>
     );
   }
@@ -59,50 +61,60 @@ this.setState({
 
 
 class App extends Component {
-  constructor(props){
-  super(props);
-  this.state=({
-    note:"",
-    items:[]
-  });
-  this.displayText=this.displayText.bind(this);
-}
+  constructor(props) {
+    super(props);
+    this.state = ({
+      note: "",
+      items: []
+    });
+    this.displayText = this.displayText.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+  }
 
-displayText(text){
-  this.setState({
-    note:text,
-    items:[...this.state.items,text]
-  })
-  }               
+  displayText(text) {
+    this.setState({
+      note: text,
+      items: [...this.state.items, text]
+    })
+  }
 
-  
+  deleteItem(id) {
+    const newList = this.state.items.filter(item => item.id !== id);
+    console.log("id i am getting", id, newList)
+    this.setState({
+      items: [...this.state.items, newList]
+    })
+  }
 
-  render(){
+
+
+  render() {
+    {console.log("This is under render method")}
     return (
       <div className="todo">
-      <Todo takeContext={this.displayText}/>
+        {console.log("Thid is app component")}
+        <Todo takeContext={this.displayText} />
         <div>your list is added here:
-          
-          {this.state.items.map((item,index)=>(
-            <div  key={index} > 
-           
-              <input type="checkbox" className="unche"/>
-              <span>{item}</span>
-             
-            </div>
 
-          ))}
-          <div>
-           <button>
-             Remove
-          </button>
+          {this.state.items.map((item, index) => (
+          <div key={index} >
+            {console.log("This is console from displaying")}
+
+            <input type="checkbox" className="unche" />
+            <span>{item}</span>
+            <button onClick={() => this.deleteItem(item.id)}>{console.log("Component id", item.id)}Remove</button>
+
+
           </div>
+
+        ))}
+
         </div>
-        
+
       </div>
 
     );
 
-          }
-        }
+  }
+}
 export default App;
